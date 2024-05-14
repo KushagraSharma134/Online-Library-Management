0d54424349bd4806b8d730eb1265e99d1b10a1aa
Kushagra Sharma <135498075+KushagraSharma134@users.noreply.github.com>
2024-05-14 15:45:48 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,62 @@
+     <!-- FOOTER -->
+     <footer id="footer">
+          <div class="container">
+               <div class="row">
+
+                    <div class="col-md-4 col-sm-6">
+                         <div class="footer-info">
+                              <div class="section-title">
+                                   <h2>Address</h2>
+                              </div>
+                              <address>
+                                   <p>SKIT College,<br>Ramnagariya, Jagatpura,<br> Jaipur - 302017</p>
+                              </address>
+
+                              <ul class="social-icon">
+                                   <li><a href="#" class="fa fa-facebook-square" attr="facebook icon"></a></li>
+                                   <li><a href="#" class="fa fa-twitter"></a></li>
+                                   <li><a href="#" class="fa fa-instagram"></a></li>
+                              </ul>
+                         </div>
+                    </div>
+
+                    <div class="col-md-4 col-sm-6">
+                         <div class="footer-info">
+                              <div class="section-title">
+                                   <h2>Contact Info</h2>
+                              </div>
+                              <address>
+                                   <p>+91 7339872983,<br>+91 6367035116</p>
+                                   <p><a href="mailto:youremail.co">kushagrasharma.jpr2002@gmail.com</a></p>
+                              </address>
+                         </div>
+                    </div>
+
+                    <div class="col-md-4 col-sm-12">
+                         <div class="footer-info newsletter-form">
+						 
+						 
+                              <div class="copyright-text"> 
+                                   <p>Copyright &copy; <?php echo date("Y"); ?> Online Library Management</p>
+                                   
+                                   <p>Designed by Kushagra and Lakshay</p>
+                              </div>
+							  
+                         </div>
+                    </div>
+                    
+               </div>
+          </div>
+     </footer>
+
+
+     <!-- SCRIPTS -->
+     <script src="js/jquery.js"></script>
+     <script src="js/bootstrap.min.js"></script>
+     <script src="js/owl.carousel.min.js"></script>
+     <script src="js/smoothscroll.js"></script>
+     <script src="js/custom.js"></script>
+     <script src="js/jquery.dataTables.min.js"></script>
+
+</body>
+</html>
